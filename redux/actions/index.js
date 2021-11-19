a03c38a2f7de25eb202caa0b27c7cf54e759ac51
Paddybaba <53314770+Paddybaba@users.noreply.github.com>
2021-11-19 13:07:25 +0530

    Add files via upload

---
+++ redux/actions/index.js
@@ -0,0 +1,28 @@
+export const userLogin = (user) => {
+  // console.warn("data received in action", user);
+  return {
+    type: "USER_LOGIN",
+    payload: user,
+  };
+};
+
+export const selectOptions = (options) => {
+  return {
+    type: "SELECT_OPTIONS",
+    payload: options,
+  };
+};
+
+export const setQuestions = (questions) => {
+  return {
+    type: "SET_QUESTIONS",
+    payload: questions,
+  };
+};
+
+export const setTotalQuestions = (length) => {
+  return {
+    type: "SET_TOTAL_QUESTIONS",
+    payload: length,
+  };
+};
