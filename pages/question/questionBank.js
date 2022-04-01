b3d678747f322bc0d952572acd3376a13012aeba
unknown <paddybaba@gmail.com>
2022-04-01 13:38:22 +0530

    Adding Question Modal

--- pages/question/questionBank.js
+++ pages/question/questionBank.js
@@ -4,21 +4,25 @@ import { connect } from "react-redux";
 import path from "../api/mypaths";
 import QuestionCard from "../../src/components/cards/QuestionCard";
 import QbankNB from "../../src/components/navbars/QbankNB";
-
+import QuestionModal from "../../src/components/modals/QuestionModal";
 const myQuestionBank = (props) => {
   const { teacher_id } = props.teacher;
   const [myQuestions, setMyQuestions] = useState([]);
-
+  const [showQModal, setShowQModal] = useState(false)
   // Call getmyquestions and update myQuestion array on load
   useEffect(async () => {
     const data = await getMyQuestions(teacher_id);
     setMyQuestions(data);
   }, []);
 
+  function handleClose () {
+    setShowQModal(false)
+}
   return (
     <div>
+      <QuestionModal show={showQModal} handleClose={handleClose}/>
       <QbankNB />
-      <div className="qbank_container">
+      <div className="qbank_container" onClick={()=> setShowQModal(true)}>
         {myQuestions.map((item, index) => {
           return <QuestionCard item={item} key={index} />;
         })}
