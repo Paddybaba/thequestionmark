e2703e4daf37a80e19f736fa237bc14d66652f81
unknown <paddybaba@gmail.com>
2022-05-03 19:13:32 +0530

    path changed

--- pages/api/mypaths.js
+++ pages/api/mypaths.js
@@ -1,3 +1,3 @@
-// const path = "https://quiz-server-paddy.herokuapp.com";
-const path = "http://localhost:8080";
+const path = "https://quiz-server-paddy.herokuapp.com";
+// const path = "http://localhost:8080";
 export default path;
